6acdb22154a0acbaf108b80077277e4d49c10a97
zakariyaahmed123 <118251771+zakariyaahmed123@users.noreply.github.com>
2022-12-08 10:15:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+/ Write your solution in this file!
+var customerName = "bob"
+var bestCustomer = undefined
+const leastFavoriteCustomer = "You can't change me"
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer(){
+  bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "Wait a minute I shoudn't work. "
+}
+
+
